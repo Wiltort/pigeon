ce16fbd7bb673ea4c970136c2f3787f6f1d274bd
Wiltort <wiltort21@gmail.com>
2024-10-16 22:10:22 +0500

    db connection

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,21 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from decouple import config
 
 
-#DB_HOST = "localhost"
-#DB_PORT = 5432
-#DB_USER = "postgres"
-#DB_PASS = "postgres"
-#DB_NAME = "pigeon"
+db_host = config("DB_HOST")
+db_port = config("DB_PORT")
+db_user = config("DB_USER")
+db_pass = config("DB_PASS")
+db_name = config("DB_NAME")
 
-DATABASE_URL = f"postgresql+asyncpg"
+DATABASE_URL = f"postgresql+asyncpg://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession,
+                                   expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
