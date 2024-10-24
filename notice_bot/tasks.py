dae28e0f296c2eb8e297a5b705bae0a1d56a7d44
Wiltort <wiltort21@gmail.com>
2024-10-24 08:59:17 +0500

    aiosqlite

--- notice_bot/tasks.py
+++ notice_bot/tasks.py
@@ -4,7 +4,7 @@ from config import get_tg_token
 from aiogram.types import Message
 from aiogram.filters import Command
 
-import sqlite3
+import aiosqlite
 
 
 celery_app = Celery("tasks", broker="redis://localhost:6379/0")
@@ -14,26 +14,26 @@ bot = Bot(token=TELEGRAM_BOT_TOKEN)
 router = Router()
 dp = Dispatcher()
 
-conn = sqlite3.connect("users.db")
-cursor = conn.cursor()
-
-cursor.execute(
-    """
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        username TEXT UNIQUE
-    )
-"""
-)
-conn.commit()
+async def init_db():
+    async with aiosqlite.connect("users.db") as db:
+        await db.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                username TEXT UNIQUE
+            )
+            """
+        )
+        await db.commit()
 
 async def start_command(message: Message):
     user_id = message.from_user.id
     username = message.from_user.username
-    cursor.execute(
-        "INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)", (user_id, username)
-    )
-    conn.commit()
+    async with aiosqlite.connect("users.db") as db:
+        await db.execute(
+            "INSERT OR IGNORE INTO users (id, username) VALUES (?, ?)", (user_id, username)
+        )
+        await db.commit()
 
     await message.reply("Ваши данные сохранены.")
 
@@ -41,10 +41,11 @@ router.message.register(start_command, Command(commands=["start"]))
 dp.include_router(router)
 
 
-def get_user_id_by_username(username):
-    cursor.execute("SELECT id FROM users WHERE username = ?", (username,))
-    result = cursor.fetchone()
-    return result[0] if result else None
+async def get_user_id_by_username(username):
+    async with aiosqlite.connect("users.db") as db:
+        async with db.execute("SELECT id FROM users WHERE username = ?", (username,)) as cursor:
+            result = await cursor.fetchone()
+            return result[0] if result else None
 
 
 @celery_app.task
@@ -52,3 +53,6 @@ async def send_telegram_notification(username: str, message: str):
     """Послать юзеру сообщение в телеграм по его логину."""
     user_id = get_user_id_by_username(username=username[1:])
     await bot.send_message(chat_id=user_id, text=message)
+
+import asyncio
+asyncio.run(init_db())
