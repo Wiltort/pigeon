74a81b8093c6267ee64b5f524c455ac04737be86
Wiltort <wiltort21@gmail.com>
2024-10-24 13:34:19 +0500

    sdsd

--- notice_bot/tasks.py
+++ notice_bot/tasks.py
@@ -27,7 +27,6 @@ async def init_db():
         await db.commit()
 
 
-@router.message(Command=["start"])
 async def start_command(message: Message):
     user_id = message.from_user.id
     username = message.from_user.username
@@ -39,7 +38,7 @@ async def start_command(message: Message):
 
     await message.reply("Ваши данные сохранены.")
 
-# router.message.register(start_command, Command(commands=["start"]))
+router.message.register(start_command, Command(commands=["start"]))
 dp.include_router(router)
 
 
